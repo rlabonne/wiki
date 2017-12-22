f2f1fab51cb20c988bf5ce21ce661214d1da9034
Rob LaBonne <roblabonne@gmail.com>
2017-12-22 00:50:07 -0500

    ability to create new wikis added

--- app/controllers/wikiis_controller.rb
+++ app/controllers/wikiis_controller.rb
@@ -4,10 +4,27 @@ class WikiisController < ApplicationController
   end
 
   def show
+    @wikii = Wikii.find(params[:id])
   end
 
   def new
+    @wikii = Wikii.new
   end
+
+  def create
+  @wikii = Wikii.new
+  @wikii.title = params[:wikii][:title]
+  @wikii.body = params[:wikii][:body]
+  @wikii.user = current_user
+
+  if @wikii.save
+    flash[:notice] = "Wiki was saved."
+    redirect_to @wikii
+  else
+    flash.now[:alert] = "There was an error saving the wiki. Please try again."
+    render :new
+  end
+end
 
   def edit
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def current_page_style(controller, action)
     'active' if current_page?(controller: controller, action: action)
   end
+
+  def form_group_tag(errors, &block)
+    css_class = 'form-group'
+    css_class << ' has-error' if errors.any?
+    content_tag :div, capture(&block), class: css_class
+  end
 end
